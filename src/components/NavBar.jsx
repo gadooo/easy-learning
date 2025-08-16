fd2f30846bb2f33e06e47a6402b235de50156015
Mogdad Abdelrahim <mogdad19998@gmail.com>
2025-08-16 06:17:32 +0400

    create the nav bar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div className="nav-bar-contanier">
+      <ul nav-bar>
+        <li className="nav-bar-item"></li>
+        <li className="nav-bar-item"></li>
+        <li className="nav-bar-item"></li>
+        <li className="nav-bar-item"></li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
